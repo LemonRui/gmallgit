dbccb9ae02fcc8b6bce6ec8225ecbbcea4fee2b8
LemonRui <35069619+LemonRui@users.noreply.github.com>
2020-10-21 00:47:56 +0800

    更新3
    更新3

--- gmall_test/src/main/java/com/yang/gmall_test/GmallTestApplication.java
+++ gmall_test/src/main/java/com/yang/gmall_test/GmallTestApplication.java
@@ -1,11 +1,11 @@
 package com.yang.gmall_test;
 
-import org.springframework.boot.SpringApplication;
+#import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GmallTestApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GmallTestApplication.class, args);
 	}
