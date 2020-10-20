57626407f2930a5e7c0c2540f9680c4312d84d87
YangMengrui <960959230@qq.com>
2020-10-21 00:44:15 +0800

    更新2

---
+++ gmall_test/src/main/java/com/yang/gmall_test/GmallTestApplication.java
@@ -0,0 +1,13 @@
+package com.yang.gmall_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmallTestApplication.class, args);
+	}
+
+}
